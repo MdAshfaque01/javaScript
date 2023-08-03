e2af60ac32938927e446bf63ae5be97931de4cbe
Md Ashfaque <103313835+MdAshfaque01@users.noreply.github.com>
2023-08-03 15:36:53 +0000

    objects more

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,48 @@
+// const tinderUser = new Object();
+
+const tinderUser = {}
+
+tinderUser.id = "123abc";
+tinderUser.name = "sammy";
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email:"someUser@gmail.com",
+    fullName:{
+        userfullname:{
+            firstname:'hitesh',
+            lastname:'choudhary'
+        }
+    }
+}
+
+// console.log(regularUser.fullName.userfullname.firstname)
+
+const obj1 = {
+    1:"a",
+    2:"b"
+}
+const obj2 = {
+    1:"c",
+    2:"d"
+}
+
+const obj3 = {obj1,obj2}
+// console.log(obj3)
+
+const obj4 = Object.assign({},obj1,obj2);
+console.log(obj4)
+
+// spread operator
+const obj5 = {...obj1,...obj2} 
+
+
+console.log(Object.keys(tinderUser));
+
+console.log(Object.values(tinderUser));
+
+
+
+
