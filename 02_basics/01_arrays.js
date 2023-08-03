5a61df0b0059c7b5f519150860893e65d0186e34
Md Ashfaque <103313835+MdAshfaque01@users.noreply.github.com>
2023-08-03 12:13:40 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,37 @@
+const myArr = [0,1,2,3,4,5];
+
+const myHeros = ["shaktimaan","Naagraj"];
+
+const myArr2 = new Array(1,2,4,5);
+
+
+// Array methods
+
+myArr.push(12);
+myArr.push(7);
+
+// console.log(myArr)
+
+myArr.pop()
+
+// console.log(myArr)
+
+
+// myArr.unshift(0);
+// console.log(myArr)
+
+// myArr.shift();
+// console.log(myArr)
+
+
+// console.log(myArr.includes(9));
+
+// console.log(myArr.indexOf(3));
+
+
+// slice , splice
+
+console.log("A ",myArr);
+
+const myn1 = myArr.slice(1,3);
+
