66b80f6ce95d9e16ba5d356495e7b88c7a68fa10
Md Ashfaque <103313835+MdAshfaque01@users.noreply.github.com>
2023-08-03 15:19:23 +0000

    objects started

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,14 @@
+
+const user = {
+   name: "Ashfaque",
+   age : 23,
+   location : "Jharkhan",
+   email:"ashfaque@gmail.com",
+   isLoggedIn:false,
+   lastLoggedInDays:["Monday","Sunday"]
+};
+
+console.log(user.email);
+console.log(user["email"]);
+
+
